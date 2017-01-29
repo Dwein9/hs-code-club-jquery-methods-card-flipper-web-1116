456e01ed1a348aa587960a15c19f84b462203180
David Weinstein <txea@Davids-MacBook-Air.local>
2017-01-29 13:31:49 -0500

    done done done

--- js/card.js
+++ js/card.js
@@ -1,11 +1,17 @@
 $( function(){
 
   $( "#card-one").click(function(){
-    //Use toggleClass method here
+    $(this).toggleClass("flipped")
   });
 
-
 	// code click event for card-two here
-
+  $( "#card-two").click(function(){
+    $(this).toggleClass("flipped")
+  });
   // code click event for card-three here
+  $( "#card-three").click(function(){
+    $(this).toggleClass("flipped")
+  });
  })
+// The click event should use the toggleClass method. If the card is on the front, it should add the class of flipped.
+// If it's on the back, it should remove that class.
